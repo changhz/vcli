e98e5683899f806e1e4abec73d02f3605d295e37
changhz <hz.chang@pm.me>
2022-12-15 06:50:45 -0500

    vcli

---
+++ README.md
@@ -0,0 +1,3 @@
+# vcli
+
+Boilerplate for making V CLI tools.
--- vcli.v
+++ vcli.v
@@ -1,5 +1,41 @@
 module main
 
+import os
+import flag
+
 fn main() {
-	println('Hello World!')
+  mut fprs := flag.new_flag_parser(os.args)
+  fprs.application('vcli')
+  fprs.version('0.0.1')
+  fprs.description('Boilerplate for making V CLI tools')
+  fprs.skip_executable()
+
+  mut prjname := fprs.string('name', `n`, '', "Project Name").to_lower()
+
+  if prjname == '' {
+    prjname = get_input()
+    if 'c' == prjname {
+      return
+    }
+  }
+
+  println("Nice, your project is called '$prjname' !")
+}
+
+fn get_input() string {
+  for true {
+    input_str := (os.input_opt('Please enter project name:\n') or { '' }).to_lower()
+    
+    if 'c' == input_str {
+      return 'c'
+    }
+
+    if '' == input_str {
+      println("Invalid name (enter 'c' to abort)")
+      continue
+    }
+
+    return input_str
+  }
+  return 'c'
 }
